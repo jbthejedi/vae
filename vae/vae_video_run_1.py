e3e23b14298582d339064c0dc03efb07700062d0
Justin Barry <justinbarrythejedi@gmail.com>
2025-06-12 19:00:05 +0700

    added vae-conv

--- vae/vae_video_run_1.py
+++ vae/vae_video_run_1.py
@@ -19,8 +19,108 @@ from omegaconf import OmegaConf
 
 
 class VAEConv(nn.Module):
-    pass
+    def __init__(
+        self, in_channels=3, out_channels=3,
+        latent_dims=128, p_dropout=0.1, image_size=224
+    ):
+        """
+        64 -> 128 -> 256 -> 256
+        mu: latent_dims
+        logvar: latent_dims
+        """
+        super().__init__()
+        self.image_size = image_size
+        self.conv1 = DoubleConv(in_channels, 64, p_dropout)           # -> (,64,224,224)
+        self.pool1 = nn.MaxPool2d(2)                       # -> (,64,112,122) 
 
+        self.conv2 = DoubleConv(64, 128, p_dropout)                   # -> (,128,112,112)
+        self.pool2 = nn.MaxPool2d(2)                       # -> (,128,56,56) 
+
+        self.conv3 = DoubleConv(128, 256, p_dropout)                  # -> (,256,56,56)
+        self.pool3 = nn.MaxPool2d(2)                       # -> (,256,28,28) 
+
+        # Refine latent features
+        self.conv4 = DoubleConv(256, 256, p_dropout)                  # -> (,256,28,28)
+        C, H, W = self._get_flattened_dims(in_channels)
+        self.flattened_dims = C*H*W
+        self.chw = C, H, W
+        self.fc_mu = nn.Linear(in_features=self.flattened_dims, out_features=latent_dims)
+        self.fc_logvar = nn.Linear(in_features=self.flattened_dims, out_features=latent_dims)
+
+        self.fc_up = nn.Linear(latent_dims, self.flattened_dims) # -> (latent_dims, C*H*W)
+        self.up3 = nn.ConvTranspose2d(256, 128, 2, stride=2)
+        self.bn3 = nn.BatchNorm2d(num_features=128)
+
+        self.up2 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+        self.bn2 = nn.BatchNorm2d(num_features=64)
+
+        self.up1 = nn.ConvTranspose2d(64, 64, 2, stride=2)
+        self.bn1 = nn.BatchNorm2d(num_features=64)
+
+        self.head = nn.ConvTranspose2d(64, out_channels, 1, stride=1)
+        # 256 -> 128 -> 64 -> 64
+    
+    
+    def _get_flattened_dims(self, in_channels):
+        with torch.no_grad():
+            dummy = torch.zeros(1, in_channels, self.image_size, self.image_size)
+            x : torch.Tensor = self.pool1(self.conv1(dummy))
+            x = self.pool2(self.conv2(x))
+            x = self.pool3(self.conv3(x))
+            x = self.conv4(x)
+            _, C, H, W = x.shape
+            return C, H, W
+
+
+    def encode(self, x):
+        x = self.pool1(self.conv1(x))
+        x = self.pool2(self.conv2(x))
+        x = self.pool3(self.conv3(x))
+        x = self.conv4(x)
+        x = torch.flatten(x, start_dim=1)
+        # x = x.view(-1, self.flattened_dims)
+        mu = self.fc_mu(x)
+        logvar = self.fc_logvar(x)
+        return mu, logvar
+    
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        recons = self.decode(z)
+        return recons, mu, logvar
+
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + std * eps
+
+
+    def decode(self, z):
+        x = torch.tanh(self.fc_up(z))
+        C, H, W = self.chw
+        x = x.view(-1, C, H, W)
+        x = torch.relu(self.bn3(self.up3(x)))
+        x = torch.relu(self.bn2(self.up2(x)))
+        x = torch.relu(self.bn1(self.up1(x)))
+        return self.head(x)
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, p_dropout=0.1):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=p_dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
 
 class VAEMlp(nn.Module):
     def __init__(self, input_dims=784, hidden_dims=256, latent_dims=16):
@@ -209,15 +309,34 @@ def load_and_test_model(config):
 
 
 def train_test_model(config):
-    dataset = datasets.MNIST(
-        root=config.data_root,
-        download=False,
-        transform=T.Compose([
-            T.ToTensor(),
-        ])
-    )
+    if config.dataset_name == 'mnist':
+        dataset = datasets.MNIST(
+            root=config.data_root,
+            download=False,
+            transform=T.Compose([
+                T.ToTensor(),
+            ])
+        )
+    elif config.dataset_name == 'cifar10':
+        dataset = datasets.CIFAR10(
+            root=config.data_root,
+            download=False,
+            transform=T.Compose([
+                T.Resize((config.image_size, config.image_size)),
+                T.ToTensor(),
+            ])
+        )
+    else:
+        raise Exception('No dataset provided')
+
     train_dl, val_dl = get_train_val_dl(dataset, config)
-    model = VAEMlp(input_dims=784, hidden_dims=256, latent_dims=16)
+    model = VAEConv(
+        in_channels=config.num_channels,
+        out_channels=3,
+        latent_dims=config.latent_dims,
+        p_dropout=config.p_dropout,
+        image_size=config.image_size)
+    # VAEConv()
     model = model.to(config.device)
     optimizer = torch.optim.AdamW(model.parameters(), lr=config.lr)
 
@@ -228,10 +347,11 @@ def train_test_model(config):
             train_loss = 0.0
             for batch_idx, (inputs, _) in enumerate(pbar):
                 inputs = inputs.to(config.device)
-                inputs = torch.flatten(inputs, start_dim=1)
+                # inputs = torch.flatten(inputs, start_dim=1)
                 recons, mu, logvar = model(inputs)
                 optimizer.zero_grad()
-                loss = vae_loss(recons, inputs, mu, logvar, F.binary_cross_entropy_with_logits)
+                # loss = vae_loss(recons, inputs, mu, logvar, F.binary_cross_entropy_with_logits)
+                loss = vae_loss(recons, inputs, mu, logvar, F.mse_loss)
                 loss.backward()
                 optimizer.step()
                 train_loss += loss.item()
@@ -244,9 +364,10 @@ def train_test_model(config):
             val_loss = 0.0
             for batch_idx, (inputs, _) in enumerate(pbar):
                 inputs = inputs.to(config.device)
-                inputs = torch.flatten(inputs, start_dim=1)
+                # inputs = torch.flatten(inputs, start_dim=1)
                 recons, mu, logvar = model(inputs)
-                loss = vae_loss(recons, inputs, mu, logvar, F.binary_cross_entropy_with_logits)
+                # loss = vae_loss(recons, inputs, mu, logvar, F.binary_cross_entropy_with_logits)
+                loss = vae_loss(recons, inputs, mu, logvar, F.mse_loss)
                 val_loss += loss.item()
                 pbar.set_postfix(val_loss=f"{val_loss / (batch_idx + 1) :.2f}")
         val_epoch_loss = val_loss / len(val_dl)
@@ -269,12 +390,14 @@ def main():
         if config.model_type == 'mlp':
             m = VAEMlp(input_dims=784, hidden_dims=256, latent_dims=16)
             input_shape = (1, 784)
-        # else:
-        #     m = VAEConv(config.num_channels, 64, config.p_dropout, config.image_size)
-        #     input_shape = (1, config.num_channels, config.image_size, config.image_size)
+        elif config.model_type == 'conv':
+            m = VAEConv(config.num_channels, 64, config.latent_dims, config.p_dropout, config.image_size)
+            input_shape = (1, config.num_channels, config.image_size, config.image_size)
+        else:
+            raise Exception("Model type for summary not provided or not valid")
         summary(
             m, 
-            input_size=input_shape,  # Add batch size here!
+            input_size=input_shape,
             col_names=["input_size", "output_size", "num_params"],
             # verbose=2
         )
